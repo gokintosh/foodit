b05a7808dccea936546a9ac99f436154c44ebc4a
gokintosh <gokul.kannambally_balachandran@student.po.edu.pl>
2020-09-09 22:52:21 +0200

    add bootstrapp

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express=require('express');
 const app=express();
+const bodyParser=require("body-parser")
 
 app.set("view engine","ejs");
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended:true}))
 // array of comics
 const comics=[
     {
-        title:"American Born Chinese",
+        title:"American Born Ce",
         description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam quae praesentium nobis omnis sit officia mollitia eligendi neque ex\ quos repellendus minima, molestiae accusantium corrupti nesciunt dolorum eius distinctio id consequuntur maiores, vitae eveniet repellat. Numquam officiis fugit at cum.",
         image:"http://www.apimages.com/Images/Ap_Creative_Stock_Header.jpg"
     },
     {
-        title:"goku the hero",
+        title:"goku the heo",
         description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam quae praesentium nobis omnis sit officia mollitia eligendi neque ex quos repellendus minima, molestiae accusantium corrupti nesciunt dolorum eius distinctio id consequuntur maiores, vitae eveniet repellat. Numquam officiis fugit at cum.",
         image:"http://www.apimages.com/Images/Ap_Creative_Stock_Header.jpg"
     },
@@ -33,6 +35,11 @@ app.get("/",(req,res)=>{
 
 app.get("/comics",(req,res)=>{
     res.render("comics",{comics})
+})
+
+app.post("/comics",(req,res)=>{
+    comics.push(req.body)
+    res.redirect("/comics")
 })
 
 app.get("/comics/new",(req,res)=>{
